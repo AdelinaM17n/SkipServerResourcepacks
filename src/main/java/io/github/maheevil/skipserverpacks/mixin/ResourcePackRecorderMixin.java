fc2e4df29f39785a272ab4c42ddf7e306d059e03
Maheesha <maheeshasithum@gmail.com>
2023-03-21 12:36:34 +0530

    Init

---
+++ src/main/java/io/github/maheevil/skipserverpacks/mixin/ResourcePackRecorderMixin.java
@@ -0,0 +1,31 @@
+package io.github.maheevil.skipserverpacks.mixin;
+
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.game.ClientboundResourcePackPacket;
+import net.minecraft.network.protocol.game.ServerboundResourcePackPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Pseudo
+@Mixin(targets = "com/replaymod/recording/packet/ResourcePackRecorder")
+public abstract class ResourcePackRecorderMixin {
+    @Shadow public abstract ServerboundResourcePackPacket makeStatusPacket(String hash, ServerboundResourcePackPacket.Action action);
+
+    @Inject(
+            method = "handleResourcePack",
+            at = @At(value = "HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    public void onResourcePackSend(Connection netManager, ClientboundResourcePackPacket packet, CallbackInfoReturnable<ClientboundResourcePackPacket> ci) {
+            String hash = packet.getHash();
+            netManager.send(this.makeStatusPacket(hash, ServerboundResourcePackPacket.Action.ACCEPTED));
+            netManager.send(this.makeStatusPacket(hash, ServerboundResourcePackPacket.Action.SUCCESSFULLY_LOADED));
+            //ci.setReturnValue();
+            ci.cancel();
+    }
+}
